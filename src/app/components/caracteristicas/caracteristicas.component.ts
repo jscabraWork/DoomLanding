93a43b4042f6e29819ec17d79921b1eaba6df483
OscarJulianRC <roncanciooscarjulian23@gmail.com>
2025-10-01 14:26:52 -0500

    caracteristicas

--- src/app/components/caracteristicas/caracteristicas.component.ts
+++ src/app/components/caracteristicas/caracteristicas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-caracteristicas',
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './caracteristicas.component.html',
   styleUrl: './caracteristicas.component.scss'
 })
-export class CaracteristicasComponent {
+export class CaracteristicasComponent implements AfterViewInit {
+  @ViewChild('video1') video1!: ElementRef<HTMLVideoElement>;
+  @ViewChild('video2') video2!: ElementRef<HTMLVideoElement>;
 
+  activeVideo: number = 1;
+
+  ngAfterViewInit() {
+    this.video1.nativeElement.play();
+  }
+
+  setActiveVideo(videoNumber: number) {
+    this.activeVideo = videoNumber;
+
+    if (videoNumber === 1) {
+      this.video1.nativeElement.play();
+      this.video2.nativeElement.pause();
+    } else {
+      this.video2.nativeElement.play();
+      this.video1.nativeElement.pause();
+    }
+  }
 }
