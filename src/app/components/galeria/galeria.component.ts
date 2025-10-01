f00e31fffb14d2080f3d10be0801b02d9c4ecf5a
Jorge Andres Riaño <jarps1994@gmail.com>
2025-10-01 13:29:25 -0500

    ajuste carrusel

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
   imports: [],
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrl: './galeria.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GaleriaComponent {
 
